47b93e0c97d5053c9696af52421918cec0ecada5
Xalovald <guilian.bahno@laposte.net>
2025-01-27 16:41:24 +0100

    CoRoutine

--- Assets/Scripts/RupeeManager.cs
+++ Assets/Scripts/RupeeManager.cs
@@ -1,16 +1,44 @@
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class RupeeManager : MonoBehaviour
 {
+    public Transform spawner;
+    public Rupee prefab;
+    public Transform container;
+    public float spawnDelay = 2f;
+    public readonly List<Rupee> _rupees = new List<Rupee>();
+    private Coroutine _spawnRoutine;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        StartSpawning();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void StartSpawning()
+    {
+        _spawnRoutine = StartCoroutine(SpawnRoutine());
+    }
+
+    private void Spawn()
+    {
+        var rupee = Instantiate(prefab, spawner.position, Quaternion.identity);
+        rupee.transform.parent = container;
+    }
+
+    private IEnumerator SpawnRoutine()
+    {
+        Spawn();
+        yield return new WaitForSeconds(spawnDelay);
+        StartSpawning();
     }
 }
